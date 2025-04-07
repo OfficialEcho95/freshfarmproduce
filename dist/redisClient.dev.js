6da1ae13d76bfc77ec118a48d75050a95ae4d15b
Emmanuel Chukwu <officialecho95@outlook.com>
2025-04-07 13:11:26 +0100

    docker compose and redisclient update

--- dist/redisClient.dev.js
+++ dist/redisClient.dev.js
@@ -1,10 +1,13 @@
 "use strict";
 
+/* eslint-disable jest/require-hook */
 var _require = require('@redis/client'),
     createClient = _require.createClient;
 
+var redisHost = process.env.REDIS_HOST || 'localhost';
+var redisPort = process.env.REDIS_PORT || 6379;
 var redisClient = createClient({
-  url: 'redis://127.0.0.1:6379'
+  url: "redis://".concat(redisHost, ":").concat(redisPort)
 });
 redisClient.connect()["catch"](function (error) {
   console.error('Redis connection error:', error);
--- redisClient.js
+++ redisClient.js
@@ -1,7 +1,12 @@
+/* eslint-disable jest/require-hook */
 const { createClient } = require('@redis/client');
 
-const redisClient = createClient({ url: 'redis://127.0.0.1:6379' });
+const redisHost = process.env.REDIS_HOST || 'localhost';
+const redisPort = process.env.REDIS_PORT || 6379;
 
+const redisClient = createClient({
+  url: `redis://${redisHost}:${redisPort}`,
+});
 
 redisClient.connect().catch((error) => {
   console.error('Redis connection error:', error);

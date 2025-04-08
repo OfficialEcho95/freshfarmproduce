208f5a64bcb183a72a1838f047bc21df878d19b0
Emmanuel Chukwu <officialecho95@outlook.com>
2025-04-08 15:29:16 +0100

    admin routes update

--- backend/admin/routes/adminRoutes.js
+++ backend/admin/routes/adminRoutes.js
@@ -9,16 +9,16 @@ const { salesReport } = require('../controllers/salesReport');
 
 const router = express.Router();
 
-router.post('/registerAdmin', registerAdmin);
+// router.post('/registerAdmin', registerAdmin);
 // router.post('/loginAdmin', loginAdmin);
 
-router.use(adminAuthenticateToken, adminAccess);
-router.get('/logout-admin', logoutAdmin);
-router.post('/admincreateuser', adminCreateUser);
-router.post('/adminupdateuser', adminUpdateUser);
-router.get('/get-user-details/:email', adminViewUserDetails);
-router.delete('/admindeleteuser', adminDeleteUser);
-router.get('/reports/complete-orders', getAllCompleteOrders);
-router.get('/reports/sales', salesReport);
+// router.use(adminAuthenticateToken, adminAccess);
+// router.get('/logout-admin', logoutAdmin);
+// router.post('/admincreateuser', adminCreateUser);
+// router.post('/adminupdateuser', adminUpdateUser);
+// router.get('/get-user-details/:email', adminViewUserDetails);
+// router.delete('/admindeleteuser', adminDeleteUser);
+// router.get('/reports/complete-orders', getAllCompleteOrders);
+// router.get('/reports/sales', salesReport);
 
 module.exports = router;

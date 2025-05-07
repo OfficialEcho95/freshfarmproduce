e938371aec47a756a29fd1e9ef34270b80b35d34
Emmanuel Chukwu <officialecho95@outlook.com>
2025-05-07 12:22:56 +0100

    reddis update

--- dist/redisClient.dev.js
+++ dist/redisClient.dev.js
@@ -1,18 +1,32 @@
 "use strict";
 
-/* eslint-disable jest/require-hook */
-var _require = require('@redis/client'),
-    createClient = _require.createClient;
+var _redis = require("@upstash/redis");
 
-var redisHost = process.env.REDIS_HOST || 'localhost';
-var redisPort = process.env.REDIS_PORT || 6379;
-var redisClient = createClient({
-  url: 'redis://localhost:6379'
+// /* eslint-disable jest/require-hook */
+// const { createClient } = require('@redis/client');
+// // const redisHost = process.env.REDIS_HOST || 'localhost';
+// // const redisPort = process.env.REDIS_PORT || 6379;
+// const redisClient = createClient({
+//   url: 'redis://localhost:6379',
+// });
+// redisClient.connect().catch((error) => {
+//   console.error('Redis connection error:', error);
+// });
+// redisClient.on('error', (error) => {
+//   console.error('Redis Client Error:', error);
+// });
+// module.exports = redisClient;
+var redis = new _redis.Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN
+}); // Set value in Redis
+
+redis.set('foo', 'bar').then(function () {
+  console.log('Data set successfully'); // Get value from Redis
+
+  return redis.get('foo');
+}).then(function (data) {
+  console.log('Data from Redis:', data); // 'bar'
+})["catch"](function (error) {
+  console.error('Error interacting with Redis:', error);
 });
-redisClient.connect()["catch"](function (error) {
-  console.error('Redis connection error:', error);
-});
-redisClient.on('error', function (error) {
-  console.error('Redis Client Error:', error);
-});
-module.exports = redisClient;
--- redisClient.js
+++ redisClient.js
@@ -1,19 +1,44 @@
-/* eslint-disable jest/require-hook */
-const { createClient } = require('@redis/client');
+// /* eslint-disable jest/require-hook */
+// const { createClient } = require('@redis/client');
 
-const redisHost = process.env.REDIS_HOST || 'localhost';
-const redisPort = process.env.REDIS_PORT || 6379;
+// // const redisHost = process.env.REDIS_HOST || 'localhost';
+// // const redisPort = process.env.REDIS_PORT || 6379;
 
-const redisClient = createClient({
-  url: 'redis://localhost:6379',
+// const redisClient = createClient({
+//   url: 'redis://localhost:6379',
+// });
+
+// redisClient.connect().catch((error) => {
+//   console.error('Redis connection error:', error);
+// });
+
+// redisClient.on('error', (error) => {
+//   console.error('Redis Client Error:', error);
+// });
+
+// module.exports = redisClient;
+
+
+
+
+import { Redis } from '@upstash/redis';
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
-redisClient.connect().catch((error) => {
-  console.error('Redis connection error:', error);
-});
+// Set value in Redis
+redis.set('foo', 'bar')
+  .then(() => {
+    console.log('Data set successfully');
 
-redisClient.on('error', (error) => {
-  console.error('Redis Client Error:', error);
-});
-
-module.exports = redisClient;
+    // Get value from Redis
+    return redis.get('foo');
+  })
+  .then((data) => {
+    console.log('Data from Redis:', data);  // 'bar'
+  })
+  .catch((error) => {
+    console.error('Error interacting with Redis:', error);
+  });
